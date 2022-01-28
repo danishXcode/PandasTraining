195db01c2a9fb3fd1569242524cf132d051d6188
danishXcode <danish77shaikh@gmail.com>
2022-01-28 21:14:56 +0530

    adding Describe

--- PandasTraining/main.py
+++ PandasTraining/main.py
@@ -4,7 +4,7 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import pandas as pd
 import numpy as np
-from datetime import datetime,timedelta
+from datetime import datetime, timedelta
 import talib.abstract as ta
 import qtpylib.indicators as qtpylib
 
@@ -13,21 +13,23 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     filename = "ETH_USDT-15m.csv"
     df = pd.read_csv(filename)
-    df.columns = ['date', 'open', 'high', 'low', 'close','volume']
+    df.columns = ['date', 'open', 'high', 'low', 'close', 'volume']
     df.date = pd.to_datetime(df['date'], unit='ms')
-    df = df.loc[df['date'] > (datetime.now() - timedelta(days=5))]
+    df = df.loc[df['date'] > (datetime.now() - timedelta(days=7))]
     bollinger = qtpylib.bollinger_bands(qtpylib.typical_price(df), window=20, stds=2)
     df["bb_lower"] = bollinger['lower']
     df["bb_mid"] = bollinger['mid']
     df["rsi"] = ta.RSI(df)
     df.loc[
-        (qtpylib.crossed_below(df["close"],df["bb_lower"])) & (df["rsi"] < 30)
-    ,'buy']=1
-    df_sell = df.loc[qtpylib.crossed_above(df["close"],df['bb_mid'])]
+        (qtpylib.crossed_below(df["close"], df["bb_lower"])) & (df["rsi"] < 30)
+        , 'buy'] = 1
+    df.loc[
+        (qtpylib.crossed_above(df["close"], df['bb_mid']))
+        & (df["rsi"] > 30), "sell"
+    ] = 1
 
     print("rsi")
-    print(df)
-
+    print(df.loc[(df["buy"] == 1) | (df["sell"] == 1)])
 
 
 # Press the green button in the gutter to run the script.
